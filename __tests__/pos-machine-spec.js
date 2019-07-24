1cdfbb399c96dac9f5e7e2b3e9a2ff780ede5eda
cyang <shmilycherish@gmail.com>
2019-07-24 12:58:01 +0800

    implement findItemByBarcode

--- __tests__/pos-machine-spec.js
+++ __tests__/pos-machine-spec.js
@@ -1,6 +1,16 @@
-    
-const printReceipt = require('../pos-machine');
+
+const {printReceipt, findItemByBarcode} = require('../pos-machine');
 
 it('sample test', () => {
     expect(printReceipt()).toBe(0);
-})
+});
+
+describe('find item by barcode', () => {
+    it('should return item details when find the item', () => {
+        expect(findItemByBarcode('0001')).toEqual({"id": "0001", "name" : "Coca Cola", "price": 3});
+    });
+
+    it('should return undefined when not find the item', () => {
+        expect(findItemByBarcode('')).toBeUndefined();
+    });
+});
--- pos-machine.js
+++ pos-machine.js
@@ -1,6 +1,23 @@
 
+const items = [
+    {"id": "0001", "name" : "Coca Cola", "price": 3},
+    {"id": "0002", "name" : "Diet Coke", "price": 4},
+    {"id": "0003", "name" : "Pepsi-Cola", "price": 5},
+    {"id": "0004", "name" : "Mountain Dew", "price": 6},
+    {"id": "0005", "name" : "Dr Pepper", "price": 7},
+    {"id": "0006", "name" : "Sprite", "price": 8},
+    {"id": "0007", "name" : "Diet Pepsi", "price": 9},
+    {"id": "0008", "name" : "Diet Mountain Dew", "price": 10},
+    {"id": "0009", "name" : "Diet Dr Pepper", "price": 11},
+    {"id": "0010", "name" : "Fanta", "price": 12}
+];
+
 function printReceipt() {
     return 0;
 }
 
-module.exports = printReceipt;
+function findItemByBarcode(barcode) {
+   return items.find(item => item.id === barcode);
+}
+
+module.exports = {printReceipt, findItemByBarcode};
